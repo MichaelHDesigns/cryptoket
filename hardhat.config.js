b61cb51065af3a142abb64dd57e0bff439675535
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-02 00:26:54 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,14 +9,14 @@ module.exports = {
     hardhat: {
       chainId: 2330,
     },
-     mumbai: {
-      url: 'https://rpc0.altcoinchain.org/rpc',
+  //   mumbai: {
+  //    url: 'https://rpc0.altcoinchain.org/rpc',
+  //    accounts: [privateKey],
+  //   },
+     goerli: {
+       url: 'https://rpc0.altcoinchain.org/rpc',
       accounts: [privateKey],
      },
-    // goerli: {
-    //   url: 'https://goerli.infura.io/v3/0e46113c66824da89ce549a3fcc4a4a7',
-    //   accounts: [privateKey],
-    // },
   },
   solidity: '0.8.4',
 };
